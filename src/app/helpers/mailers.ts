101fbc3ee9a96b086284b5fa4dd3426b1a2edd23
aryan <aryanpandit1718@gmail.com>
2024-06-29 10:30:19 +0530

    update mailers.ts file

---
+++ src/app/helpers/mailers.ts
@@ -0,0 +1,42 @@
+import nodemailer from 'nodemailer'
+import User from '@/app/models/userSchema'
+import bcryptjs from 'bcryptjs'
+
+
+export const sendMail = async ({email , emailType , userId} : any) => {
+    try {
+        const hashedToken = await bcryptjs.hash(userId.toString(),10)
+        if(emailType === 'VERIFY'){
+            await User.findByIdAndUpdate(userId,{$set:{verificationToken: hashedToken,verificationExpiry:Date() + 3600000}})
+        }
+          else if(emailType === 'RESET'){
+            await User.findByIdAndUpdate(userId,{$set:{forgotpasswordToken: hashedToken, forgotpasswordExpiry: Date.now() + 3600000}})
+        }
+
+        const transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+              user: process.env.USER,
+              pass: process.env.PASS
+            }
+          });
+            
+          const mailOptions = {
+            from: 'aryanpandit17032002@gmail.com',
+            to: email,
+            subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}${emailType === "VERIFY" ? `/verifyemail?token=${hashedToken}` :`/verifyforgot?token=${hashedToken}`}
+            </p>`
+        }
+            const mailResponse  = await transport.sendMail
+            (mailOptions);
+    
+            return mailResponse
+    
+    } catch (error : any) {
+        throw new Error(error.message)
+    }
+
+}
--- src/app/models/userSchema.ts
+++ src/app/models/userSchema.ts
@@ -31,3 +31,6 @@ const userSchema = new mongoose.Schema({
     },
 
 })
+
+const User = mongoose.models.users || mongoose.model("users",userSchema)
+export default User
