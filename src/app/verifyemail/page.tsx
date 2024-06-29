467a34a954d13c57a6796f01c678472980c0590d
Dhrit Patel <dhritpatel19@gnu.ac.in>
2024-06-29 13:42:25 +0530

    verify not working

---
+++ src/app/verifyemail/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import H1Component from '../typography/H1Component';
+
+const VerifyEmail: React.FC = () => {
+  return (
+    <div className="Button">
+      <H1Component text="Verify Your Email" />
+      <button>Verify Now</button>
+    </div>
+  );
+};
+
+export default VerifyEmail;
